19229e08e742345d8e6cbd1ce583b6b592539d04
Алексей <lexaqwas74@gmail.com>
2023-04-25 15:38:05 +0300

    Burger menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,10 @@
     const resultSelect = document.querySelector('.select__result');
     const resultRange = document.querySelector('.range-result');
     const rangeVal = document.querySelector('.range-block__input-range');
-    const options = document.querySelectorAll('.select ul li')
+    const options = document.querySelectorAll('.select ul li');
+    const layout = document.querySelector('.layout');
+    const burgerMenu = document.querySelector('.burger');
+    const header = document.querySelector('header');
 
     const removeActive = (elems) => {
         elems.forEach((elem) => {
@@ -26,6 +29,16 @@
 
     rangeVal.addEventListener('input', (e) => {
         resultRange.innerHTML = `${e.target.value} %`
+    })
+
+    burgerMenu.addEventListener('click', () => {
+        header.classList.toggle('active');
+        layout.classList.toggle('active');
+    })
+
+    layout.addEventListener('click', () => {
+        header.classList.remove('active')
+        layout.classList.remove('active')
     })
 })
 ()
